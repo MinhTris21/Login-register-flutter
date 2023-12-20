7c735dc7d955192193a692ab37a470287b860c7b
MinhTris <boyds_24854@yahoo.com.vn>
2023-12-20 09:25:10 +0700

    chap 26

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -3,7 +3,7 @@ import 'package:basic_flutter/services/auth/auth_user.dart';
 abstract class AuthProvider {
   Future<void> initialize();
   AuthUser? get currentUser;
-  Future<AuthUser?> logIn({
+  Future<AuthUser> logIn({
     required String email,
     required String password,
   });
