c104b8e1976a883baee45e26819bad1fb24f7724
MinhTris <boyds_24854@yahoo.com.vn>
2023-12-27 13:00:32 +0700

    final update

--- lib/views/forgot_password_view.dart
+++ lib/views/forgot_password_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:basic_flutter/services/auth/bloc/auth_bloc.dart';
 import 'package:basic_flutter/services/auth/bloc/auth_event.dart';
 import 'package:basic_flutter/services/auth/bloc/auth_state.dart';
