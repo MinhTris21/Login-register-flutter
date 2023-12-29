28638a5dc950a03331d73e1a0852e2dd5649ab51
MinhTris21 <105285362+MinhTris21@users.noreply.github.com>
2023-12-29 21:32:27 +0700

    Update generated_plugin_registrant.cc

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -5,7 +5,7 @@
 // clang-format off
 
 #include "generated_plugin_registrant.h"
-
+dsadasdasdads
 #include <cloud_firestore/cloud_firestore_plugin_c_api.h>
 #include <firebase_auth/firebase_auth_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
